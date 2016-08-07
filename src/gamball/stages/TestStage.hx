c7244e00d55319404b7399530a89078c38768c1f
Chuan Liu <chuanliu87@gmail.com>
2016-08-07 01:03:45 +0100

    Added some fonts in.

--- src/gamball/stages/TestStage.hx
+++ src/gamball/stages/TestStage.hx
@@ -1,6 +1,7 @@
 package gamball.stages;
 import engine.debug.Measurer;
 import engine.entities.Stage;
+import gamball.ui.Fonts;
 
 class TestStage extends Stage
 {
@@ -11,5 +12,15 @@ class TestStage extends Stage
 		testObj.x = GamBall.screenWidth * 0.5;
 		testObj.y = GamBall.screenWidth * 0.5;
 		addChild(testObj);
+		
+		Fonts.load(function():Void
+		{
+			var bitmapText = Fonts.getFancyText("Blah blah blah", 32, Fonts.CALIBRI_64_BOLD);
+			bitmapText.x = 100;
+			bitmapText.y = 300;
+			addChild(bitmapText);
+		});
+		
+		
 	}
 }
---
+++ src/gamball/ui/Fonts.hx
@@ -0,0 +1,33 @@
+package gamball.ui;
+import engine.entities.FancyBitmapText;
+import pixi.loaders.Loader;
+import pixi.loaders.LoaderOptions;
+
+class Fonts
+{
+	static public inline var CALIBRI_32_BOLD:String = "Calibri 32 Bold";
+	static public inline var CALIBRI_64_BOLD:String = "Calibri 64 Bold";
+	static public inline var CONSOLAS_NUMBERS_72_BOLD:String = "Consolas Numbers 72 Bold";
+	static public inline var LUCIDA_CONSOLE_14_BOLD:String = "Lucida Console 14 Bold";
+	
+	private function new() {}
+	
+	static public function load(?callback:Void->Void):Void
+	{
+		var loadOption:LoaderOptions = { crossOrigin: false };
+		
+		new Loader()
+		.add("assets/fonts/calibri_32_bold.fnt", loadOption)
+		.add("assets/fonts/calibri_64_bold.fnt", loadOption)
+		.add("assets/fonts/consolas_numbers_72_bold.fnt", loadOption)
+		.add("assets/fonts/lucida_console_14_bold.fnt", loadOption)
+		.load(callback);
+	}
+	
+	static public function getFancyText(text:String, size:Int, font:String, ?colour:Int, isAutoCentred:Bool = true):FancyBitmapText
+	{
+		if (colour == null) colour = 0xFFFFFF;
+		
+		return new FancyBitmapText(text, { font: Std.string(size) + "px " + font, tint:colour }, isAutoCentred);
+	}
+}
