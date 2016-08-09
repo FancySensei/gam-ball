e13d4add27826edf7bab69bdd501c209de25457c
Chuan Liu <chuanliu87@gmail.com>
2016-08-09 06:51:27 +0100

    Sensors now working properly.

--- src/gamball/entities/BonusArea.hx
+++ src/gamball/entities/BonusArea.hx
@@ -1,11 +1,16 @@
 package gamball.entities;
 import engine.entities.GameObject;
 import engine.entities.IGameObject;
+import engine.system.Time;
 import gamball.stages.GameStage;
 import pixi.core.sprites.Sprite;
 
 class BonusArea implements IGameObject
 {
+	static private inline var SPEED:Float = 150;
+	static private inline var BOUNDS_LEFT:Float = -600;
+	static private inline var TOTAL_WIDTH:Float = (600 + 400 + 200 + 70) * 2;
+	
 	private var stage:GameStage;
 	private var sensors:Array<BonusSensor> = [];
 	
@@ -13,22 +18,55 @@ class BonusArea implements IGameObject
 	{
 		this.stage = stage;
 		
-		var spr = Sprite.fromImage(Ball.PATH + stage.gameConfig.ballConfigs.get(Ball.BALL_CANDY_ID).texture, false);
-		spr.scale.set(48 / 128);
-		var sensor = new BonusSensor(Ball.BALL_CANDY_ID, 400, 100, 0x4D90FF, spr, "bonus:x2", onCandyHit);
-		sensor.position.set(0, -200);
+		createBallSensor(Ball.BALL_CANDY_ID, -600, 600, 0x4D90FF, "bonus:x2", onCandyHit);
+		createBallSensor(Ball.BALL_8_ID, 0, 400, 0xBB4FFF, "bonus:x5", onEightHit);
+		createBallSensor(Ball.BALL_POKEMON_ID, 400, 200, 0xFF7A22, "bonus:x10", onPokemonHit);
+		
+		var crown = Sprite.fromImage("assets/textures/sprites/crown.png", false);
+		var sensor = new BonusSensor(null, 70, 100, 0xFF2740, crown, null, onCrownHit);
+		sensor.position.set(600, -200);
 		stage.gameplayLayer.addChild(sensor);
 		sensors.push(sensor);
 		
-		var spr = Sprite.fromImage(Ball.PATH + stage.gameConfig.ballConfigs.get(Ball.BALL_8_ID).texture, false);
-		spr.scale.set(32 / 128);
-		var sensor = new BonusSensor(Ball.BALL_8_ID, 200, 50, 0x4D90FF, spr, "bonus:x2", onCandyHit);
+		createBallSensor(Ball.BALL_POKEMON_ID, 670, 200, 0xFF7A22, "bonus:x10", onPokemonHit);
+		createBallSensor(Ball.BALL_8_ID, 870, 400, 0xBB4FFF, "bonus:x5", onEightHit);
+		createBallSensor(Ball.BALL_CANDY_ID, 1270, 600, 0x4D90FF, "bonus:x2", onCandyHit);
+		
+		crown = Sprite.fromImage("assets/textures/sprites/crown.png", false);
+		sensor = new BonusSensor(null, 70, 100, 0xFF2740, crown, null, onCrownHit);
+		sensor.position.set(1870, -200);
+		stage.gameplayLayer.addChild(sensor);
+		sensors.push(sensor);
+	}
+	
+	private function createBallSensor(id:String, x:Float, width:Float, colour:Int, text:String, callback:Void->Void):Void
+	{
+		var spr = Sprite.fromImage(Ball.PATH + stage.gameConfig.ballConfigs.get(id).texture, false);
+		spr.scale.set(48 / 128);
+		var sensor = new BonusSensor(id, width, 100, colour, spr, text, callback);
+		sensor.position.set(x, -200);
+		stage.gameplayLayer.addChild(sensor);
 		sensors.push(sensor);
 	}
 	
 	private function onCandyHit():Void
 	{
-		trace("bola!");
+		trace("candy!");
+	}
+	
+	private function onEightHit():Void
+	{
+		trace("888!");
+	}
+	
+	private function onPokemonHit():Void
+	{
+		trace("pokemon!");
+	}
+	
+	private function onCrownHit():Void
+	{
+		trace("crown!");
 	}
 	
 	public function fixedUpdate():Void 
@@ -42,7 +80,19 @@ class BonusArea implements IGameObject
 		}
 	}
 	
-	public function update():Void {}
+	public function update():Void
+	{
+		var dist = SPEED * Time.dt;
+		for (i in 0...sensors.length)
+		{
+			var sensor = sensors[i];
+			if (sensor.x + sensor.sensorWidth <= BOUNDS_LEFT)
+			{
+				sensor.x = TOTAL_WIDTH - sensor.sensorWidth + BOUNDS_LEFT;
+			}
+			sensor.x -= dist;
+		}
+	}
 	
 	public function lateUpdate():Void {}
 }
--- src/gamball/entities/BonusSensor.hx
+++ src/gamball/entities/BonusSensor.hx
@@ -1,16 +1,21 @@
 package gamball.entities;
 import engine.utils.MathR;
 import gamball.ui.Fonts;
+import motion.Actuate;
+import motion.easing.Expo;
+import motion.easing.Linear;
 import pixi.core.display.Container;
 import pixi.core.graphics.Graphics;
 import pixi.core.sprites.Sprite;
 
 class BonusSensor extends Container
 {
-	private var ballID:String;
+	public var sensorWidth(default, null):Float;
+	public var sensorHeight(default, null):Float;
+	
+	private var ballID:String = null;
 	private var onBallHit:Void->Void;
-	private var sensorWidth:Float;
-	private var sensorHeight:Float;
+	private var boarder:Graphics;
 	
 	public function new(ballID:String, width:Float, height:Float, colour:Int, icon:Sprite, text:String, onBallHit:Void->Void)
 	{
@@ -26,22 +31,38 @@ class BonusSensor extends Container
 		stripe.endFill();
 		addChild(stripe);
 		
+		boarder = new Graphics();
+		boarder.lineStyle(10, 0xFFFFFF);
+		boarder.drawRect( -width / 2, -height / 2, width, height);
+		boarder.position.set(width / 2, height / 2);
+		boarder.alpha = 0;
+		addChild(boarder);
+		
 		icon.anchor.set(0.5, 0.5);
-		icon.position.set(width * 0.5, height * 0.35);
+		icon.position.set(width * 0.5, height * (text != null ? 0.35 : 0.5));
 		addChild(icon);
 		
-		var bonusText = Fonts.getFancyText(text, 32, Fonts.CONSOLAS_64_BOLD);
-		bonusText.position.set(width * 0.5, height * 0.75);
-		addChild(bonusText);
+		if (text != null)
+		{
+			var bonusText = Fonts.getFancyText(text, 38, Fonts.CALIBRI_32_BOLD);
+			bonusText.position.set(width * 0.5, height * 0.75);
+			addChild(bonusText);
+		}
 	}
 	
 	public function hitTest(ball:Ball):Void
 	{
-		if (!ball.hasScored && ballID == ballID
-			&& MathR.isBetween(ball.x, x, x + sensorWidth) && MathR.isBetween(ball.y, y, y + sensorHeight))
+		if (!ball.hasScored
+			&& (ballID == null || ball.id == ballID)
+			&& MathR.isBetween(ball.y, y, y + sensorHeight)
+			&& MathR.isBetween(ball.x, x, x + sensorWidth))
 		{
 			ball.hasScored = true;
 			onBallHit();
+			boarder.alpha = 1.0;
+			Actuate.tween(boarder, 0.3, {alpha: 0}).ease(Linear.easeNone);
+			boarder.scale.set(0.9, 0.9);
+			Actuate.tween(boarder.scale, 0.3, {x: 1.0, y: 1.05}).ease(Expo.easeOut);
 		}
 	}
 }
